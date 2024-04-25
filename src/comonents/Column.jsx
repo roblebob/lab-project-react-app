fe0a0370e6548539bd68e996924e18ff44cace5d
roblebob <robert.koppisch@gmail.com>
2024-04-25 19:38:42 +0200

    styling columns

--- src/comonents/Column.jsx
+++ src/comonents/Column.jsx
@@ -1,26 +1,62 @@
 import "./Column.css";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import ItemCard from "./ItemCard";
 
 const Column = ({ data, name, color, addCard, deleteCard, edit }) => {
   const submitCard = () => {
     addCard({
-      _id: uuidv4(), 
+      _id: uuidv4(),
       status: name,
       content: "",
     });
   };
 
   return (
+    <div>
     <div className="Column" style={{ backgroundColor: color }}>
       <h2>{name}</h2>;
       {data.length !== 0 &&
         data.map((item) => {
           return (
-            <ItemCard key={item._id} item={item} deleteCard={deleteCard} edit={edit}/>
+            <ItemCard
+              key={item._id}
+              item={item}
+              deleteCard={deleteCard}
+              edit={edit}
+            />
           );
         })}
-      <button className="AddCard" onClick={submitCard}>
+      
+    </div>
+
+      <button
+        className="AddCard"
+        onClick={submitCard}
+        // style={{
+        //   minWidth: "50px",
+        //   minHeight: "10px",
+        //   borderRadius: "1rem",
+        //   backgroundColor: "transparent",
+        //   marginBottom: "5px",
+        //   textAlign: "center",
+        //   fontSize: "1.5rem",
+        // }}
+        style={{
+          minWidth: "50px",
+          minHeight: "10px",
+          borderRadius: "1rem",
+          backgroundColor: "transparent",
+          marginBottom: "5px",
+          textAlign: "center",
+          fontSize: "1.5rem",
+          position: "relative",
+          bottom: "0%",
+          backgroundColor: color,
+          margin: "5px"
+        }}
+
+
+      >
         +
       </button>
     </div>
